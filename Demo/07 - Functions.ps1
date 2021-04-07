7434939b7482ec3b79c959f2fc7fc5083ddbb752
Stephen Akuffo <sa.akuffo@gmail.com>
2021-04-06 22:37:48 -0400

    classes

--- Demo/07 - Functions.ps1
+++ Demo/07 - Functions.ps1
@@ -1,6 +1,5 @@
 #Function
-function Get-AValue($one, $two) 
-{
+function Get-AValue($one, $two) {
   return $one * $two  
 }
 
@@ -25,16 +24,104 @@ $returnValue = Get-AValue -two 12 -one 11
 function Get-BValue {
   [CmdletBinding()]
   param (
-          [Parameter( Mandatory = $true
-                      , HelpMessage = "Please enter value one"
-            )
-          ]
-          [int] $one,
-          [Parameter( Mandatory = $false
-                    , HelpMessage = "Please enter value two"
-          )]
-          [int] $two = 42
+    [Parameter(
+      Mandatory = $true, 
+      HelpMessage = "Please enter value one"
+      )][int] $one,
+    [Parameter(
+      Mandatory = $false, 
+      HelpMessage = "Please enter value two"
+      )][int] $two = 42
   )
+
+  begin {}
+
+  process {
+    return $one * $two
+  }
+
+  end {}
+}
+
+# Pass in both values
+Get-BValue -one 33 -two 42
+
+Get-BValue -one 33 
+
+Get-BValue
+
+# Get-BValue -one "x"
+
+function Get-PSFiles () {
+
+  [CmdletBinding()]
+  param (
+    [Parameter (ValueFromPipeline)]
+    $file,
+    [Parameter(
+      Mandatory = $false, 
+      HelpMessage = "Please enter value two"
+      )][int] $two = 42
+  )
+
+  begin {}
+  
+  process {
+    if ($file.Name -like "*.ps1")
+    {
+      $retval = "`tPowershell file is $($file.Name) also, the answer is $two"
+      $retval # last value with nothing else is like return $retval
+    }
+  }
+
+  end {}
   
 }
 
+
+$output = Get-ChildItem | Get-PSFiles
+$output.GetType()
+
+$output
+
+function Get-AName () {
+
+  [CmdletBinding()]
+  param (
+    [Parameter (ValueFromPipelineByPropertyName)]
+    $Name
+  )
+
+  begin {}
+  
+  process {
+    $retval = "`tYou passed in $Name"
+    return $retval
+  }
+
+  end {} 
+  
+}
+
+Get-ChildItem | Get-AName
+Get-Process | Get-AName
+
+function Show-BName () {
+  [CmdletBinding()]
+  param (
+    [Parameter (ValueFromPipelineByPropertyName)]
+    $Name
+  )
+
+  begin { Write-Host 'Here are the names!' -ForegroundColor Green }
+
+  process {
+    Write-Host "`tYou passed in " -ForegroundColor White -NoNewline
+    Write-Host $Name -ForegroundColor Yellow
+  }
+  
+  end { Write-Host 'Those were the names you passed in' -ForegroundColor Green }
+}
+
+Get-ChildItem | Show-BName
+Get-Process | Show-BName
---
+++ Demo/08 - Classes.ps1
@@ -0,0 +1,10 @@
+# Define the valid values for the enum
+Enum MyTwitters
+{
+  Sakuffo
+  saku
+  stephen
+}
+
+# Get the list of names out of the thing
+[MyTwitters].GetEnumNames()
